25abc3e76edaf03cec237daa4daf7cffc3128a3e
zhangyunyun <yunzhongshu_1@163.com>
2019-01-10 17:46:19 +0800

    first commit

---
+++ demo-admin-client/src/main/java/com/zmdj/client/DemoAdminClientApplication.java
@@ -0,0 +1,15 @@
+package com.zmdj.client;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAdminClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAdminClientApplication.class, args);
+	}
+
+}
+
---
+++ demo-boot/src/main/java/com.zmdj/config/SecurityPermitAllConfig.java
@@ -0,0 +1,21 @@
+package com.zmdj.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author zhangyunyun
+ * @date 2019/1/10
+ */
+@Configuration
+public class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().anyRequest().permitAll()
+                .and().csrf().disable();
+
+    }
+}
